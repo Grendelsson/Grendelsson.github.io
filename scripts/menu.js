1d0b1098f94ed90f2b91552cf2da52d6a46adb4f
Grendelsson <Grendelsson@users.noreply.github.com>
2016-03-17 22:33:25 +0000

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,10 @@
 //Scripts for popout menu interaction
 
-window.onclick = function(elem){
+window.onclick = (function(elem){
+  console.log("function");
   var tabName = elem.target.id;
   if(tabName == "burger"){
     setTimout(elem.style.left = 100 + "px", 300);
     console.log(elem.style);
   }
-}
+})();
