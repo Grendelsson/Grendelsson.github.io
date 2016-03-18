21bba6d6122a4ce06e11f4a916a7782d6cc8038d
Grendelsson <Grendelsson@users.noreply.github.com>
2016-03-18 08:55:48 +0000

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,14 @@
 //Scripts for popout menu interaction
 
-window.onclick = (function(elem){
-  console.log("function");
-  var tabName = elem.target.id;
-  if(tabName == "burger"){
-    setTimout(elem.style.left = 100 + "px", 300);
-    console.log(elem.style);
-  }
-})();
+window.onclick = function(elem){
+if(elem.target.id == "burger" || elem.target.id =="BURGER"){
+		animate(document.getElementById("Menu"),"left","px",-200,10,100);
+		animate(document.getElementById("tab"),"left","px",0,210,100);
+		elem.target.id = "closeburger";
+	}
+	else if(elem.target.id == "closetab" || elem.target.id =="closeBURGER"){
+		animate(document.getElementById("Menu"),"left","px",10,-200,100);
+		animate(document.getElementById("closetab"),"left","px",210,0,100);
+		elem.target.id = "burger";
+	}
+}
