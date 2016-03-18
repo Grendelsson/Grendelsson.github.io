a373d39b2f5f3a5b83febf4e8fcd80c11af338a9
Grendelsson <Grendelsson@users.noreply.github.com>
2016-03-18 13:33:20 +0000

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,12 +1,12 @@
 //Scripts for popout menu interaction
 
 window.onclick = function(elem){
-if(elem.target.id == "burger" || elem.target.id =="BURGER"){
+if(elem.target.id == "burger"){
 		animate(document.getElementById("Menu"),"left","px",-200,10,100);
 		animate(document.getElementById("tab"),"left","px",0,210,100);
 		elem.target.id = "closeburger";
 	}
-	else if(elem.target.id == "closetab" || elem.target.id =="closeBURGER"){
+	else if(elem.target.id == "closeburger"){
 		animate(document.getElementById("Menu"),"left","px",10,-200,100);
 		animate(document.getElementById("closetab"),"left","px",210,0,100);
 		elem.target.id = "burger";
