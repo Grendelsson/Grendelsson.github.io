dfad733d008ad1f45b6299029559e96b0b28dcfd
Grendelsson <Grendelsson@users.noreply.github.com>
2016-03-24 17:11:34 +0000

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -13,13 +13,13 @@ function animate(elem,style,unit,from,to,time) {
 
 window.onclick = function(elem){
 if(elem.target.id == "burger"){
-		animate(document.getElementById("burger"),"top","em",0,10.0,10.0);
-		animate(document.getElementById("menu"),"top","em",-10.0,0,10.0);
+		animate(document.getElementById("burger"),"top","em",0,15,15);
+		animate(document.getElementById("menu"),"top","em",-15,0,15);
 		elem.target.id = "closeburger";
 	}
 	else if(elem.target.id == "closeburger"){
-		animate(document.getElementById("closeburger"),"top","em",10.0,0,10.0);
-		animate(document.getElementById("menu"),"top","em",0,-10.0,10.0);
+		animate(document.getElementById("closeburger"),"top","em",15,0,15);
+		animate(document.getElementById("menu"),"top","em",0,-15,15);
 		elem.target.id = "burger";
 	}
 }
