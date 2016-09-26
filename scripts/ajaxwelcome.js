6aab4d69a73beda407a469f0fc9de8d0e4cca960
Grendelsson <Grendelsson@users.noreply.github.com>
2016-09-26 17:31:00 +0100

    Update ajaxwelcome.js

--- scripts/ajaxwelcome.js
+++ scripts/ajaxwelcome.js
@@ -2,9 +2,9 @@ function welcomeNote(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
-      document.getElementById("inputTooltip").innerHTML = document.getElementById("nameinput").value;
+      document.getElementById("inputTooltip").innerHTML = xhttp.reponseText + document.getElementById("nameinput").value;
       }
      };
-     xhttp.open("POST", "welcome.txt", true);
+     xhttp.open("GET", "welcome.txt", true);
      xhttp.send();
    }
