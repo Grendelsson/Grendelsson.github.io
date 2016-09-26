242bceb324d8d09d359acd2726db4b5d63255c1a
Grendelsson <Grendelsson@users.noreply.github.com>
2016-09-26 17:18:52 +0100

    Update ajaxwelcome.js

--- scripts/ajaxwelcome.js
+++ scripts/ajaxwelcome.js
@@ -6,6 +6,6 @@ function welcomeNote(){
       }
      };
      xhttp.open("POST", "welcome.txt", true);
-     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-     xhttp.send("fname="+document.getElementById("nameinput").value);
+     xhttp.setRequestHeader("Content-type", "text");
+     xhttp.send(document.getElementById("nameinput").value);
    }
