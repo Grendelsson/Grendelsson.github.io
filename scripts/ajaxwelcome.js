2be856e08d4e3b7c7ef87e8b49359fd27fa6c55f
Grendelsson <Grendelsson@users.noreply.github.com>
2020-03-11 09:10:35 +0000

    Update ajaxwelcome.js

--- scripts/ajaxwelcome.js
+++ scripts/ajaxwelcome.js
@@ -2,7 +2,13 @@ function welcomeNote(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
-      document.getElementById("inputTooltip").innerHTML = "Hi there " + document.getElementById("nameinput").value;
+      var cid = "";
+      for(var i of dataLayer){
+        if (i.exp_cid){
+          cid = i.exp_cid);
+        }
+      }
+      document.getElementById("inputTooltip").innerHTML = "Hi there " + cid//getElementById("nameinput").value;
       }
      };
      xhttp.open("GET", "", true);
