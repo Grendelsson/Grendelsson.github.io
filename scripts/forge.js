4e538f0202fa5b071820baf48c62608dcf75747c
Grendelsson <Grendelsson@users.noreply.github.com>
2020-03-20 15:35:54 +0000

    Build chart

--- scripts/forge.js
+++ scripts/forge.js
@@ -120,6 +120,7 @@ if(intersects(myRectangle.x, myRectangle.y, e.x, e.y, myRectangle.width, myRecta
 }
 
 //Test d3 use
+//build frame
 var svgWidth = 500;  
 var svgHeight = 300;
 
@@ -127,3 +128,26 @@ var svg = d3.select('svg')
     .attr("width", svgWidth)  
     .attr("height", svgHeight)  
     .attr("class", "bar-chart");
+
+//build chart
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var barPadding = 5;  
+var barWidth = (svgWidth / dataset.length);
+
+var barChart = svg.selectAll("rect")  
+    .data(dataset)  
+    .enter()  
+    .append("rect")  
+    .attr("y", function(d) {  
+        return svgHeight - d  
+    })  
+    .attr("height", function(d) {  
+        return d;  
+    })  
+    .attr("width", barWidth - barPadding)  
+    .attr("transform", function (d, i) {  
+         var translate = [barWidth * i, 0];  
+         return "translate("+ translate +")";  
+    });
+//
